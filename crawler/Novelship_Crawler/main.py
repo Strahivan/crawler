b1fec73735637068415eff423c3bab974d05ea4c
Strahi <strahinja_ivanovic@daimler.com>
2017-10-26 21:23:10 -0700

    dynamical URL-parse-crawl

---
+++ crawler/Novelship_Crawler/main.py
@@ -0,0 +1,50 @@
+from flask import Flask
+import scrapy
+from spiders.dynamicSpider import dynamicSpider
+from scrapy.crawler import CrawlerProcess, CrawlerRunner
+
+from twisted.internet import reactor
+import os
+from subprocess import call
+
+app = Flask(__name__)
+spi = dynamicSpider()
+process = CrawlerProcess()
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/crawl/<string:domain>/<string:account_id>')
+def crawlSeller(domain, account_id):
+    crawlURL = 'https://'+domain+ '.com/'+account_id
+    process.crawl(spi, start_urls=['https://'+domain+ '.com/'+account_id])
+    process.start()
+        
+    #To-Do send .json response
+
+    #runner = CrawlerRunner()
+    #runner.crawl(spi, start_urls=['https://'+domain+ '.com/'+account_id])
+    #d = runner.join()
+    #d.addBoth(lambda _: reactor.stop())
+
+    #spi.start_urls=str(crawlURL)
+    #os.system("scrapy crawl dynamic")
+    #os.system("scrapy crawl dy -a start_requests="+str(crawlURL))
+
+    #baue crawlURL zusammen
+    #https://carousell.com/allen895/
+    #spi.start_req(domain,account_id)
+    return 'crawled:'+crawlURL
+
+
+#spider.start_urls = ['http://google.com']
+
+
+#scrapy crawl dmoz -a start_requests="this"
+
+#call(["ls", "-l"])
+
+#subprocess.call(crop, shell=True)
+
+#os.system('your_command')
---
+++ crawler/Novelship_Crawler/spiders/dynamicSpider.py
@@ -0,0 +1,39 @@
+import scrapy
+from scrapy.http import Request
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from Novelship_Crawler.items import NovelshipCrawlerItem
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from scrapy_splash import SplashRequest
+
+class dynamicSpider(CrawlSpider):
+    name = 'dynamic'
+    allowed_domains = ['carousell.com']
+    start_urls = ['https://carousell.com/khvnmrkt/']#, 'https://carousell.com/allen895/']
+
+    rules = (Rule (LinkExtractor(allow=(''),
+    restrict_xpaths=('//*[@id="productCardThumbnail"]'))
+    ,callback="parse_items", follow=True),)
+
+
+    #The next To-Do is to click on the "next-page" button and crawl
+    # xPath for next-page button:
+    #/html/body/div[2]/div/div[1]/div[2]/div/div[1]/div[2]/div[3]/ul/li[2]/a
+
+    def parse_items(self, response):
+        hxs = HtmlXPathSelector(response)
+        items = []
+        desc = hxs.select("//html/body/div[2]/div/div[1]/div[2]/div/div[3]/div[2]/section/div/div/div[1]/article")
+
+        for desc in desc:
+            item = NovelshipCrawlerItem()
+            item ["title"] = desc.select("h1/text()").extract()
+            item ["price"] = desc.select("div/div[2]/span/text()").extract()
+            item ["customer_id"] = desc.select("div/div[1]/a/text()").extract()
+            item ["location"] = desc.select("div/div[3]/span/text()").extract()
+            items.append(item)
+            return items
+
+
+#def start_req(self, domain, account_id):
+#    yield Request(crawlURL, self.parse_items)
